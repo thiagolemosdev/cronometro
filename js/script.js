afc40d3ae38fa2c8037f971b179361c992ad9c77
Thiago Lemos <lemoos.th@gmail.com>
2022-02-02 01:37:44 -0300

    Front pronto

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Mude a cor da tela para azul e depois para vermelho a cada 2s.
+
+// Crie um cronometro utilizando o setInterval. Deve ser possível
+// iniciar, pausar e resetar (duplo clique no pausar).
